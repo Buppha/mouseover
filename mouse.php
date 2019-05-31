8cbcf4289f30c20fbf0054cb492ec21ebd65042d
Buppha <34181613+Buppha@users.noreply.github.com>
2019-05-31 16:08:54 +0700

    Add files via upload

---
+++ mouse.php
@@ -0,0 +1,112 @@
+
+<html lang="th">
+<head>
+	<meta charset="UTF-8">
+	<title>Mouse Over</title>
+	<meta name="description" content="A.P. Honda ผู้นำด้านจักรยานยนต์อันดับ 1 พร้อมให้คุณค้นหาประสบการณ์ใหม่ ได้แล้ววันนี้ ที่ศูนย์ฮอนด้าทั่วประเทศไทย">
+	<meta name="keywords" content="A.P. Honda,AP Honda,Honda Motorcycle Thailand,Honda,Big bike,Bigwing,Motorcycle,Freedom,Automatic,PGM-Fi,รถจักรยายนต์,มอไซต์,มอเตอร์ไซต์,mocyc,MocycThailand,รถ มอเตอร์ไซค์ ใหม่,มอเตอร์ไซค์รุ่นใหม่,สมัครงาน,สมัครงาน Honda,สมัครงาน A.P. Honda,สมัครงาน เอพี ฮอนด้า,สมัครงาน เอ.พี. ฮอนด้า,APHonda">
+	<meta property="og:title" content="มอเตอร์ไซค์ Honda หลากหลายรุ่น หน้าหลัก | A.P. Honda">
+	<meta property="og:description" content="A.P. Honda ผู้นำด้านจักรยานยนต์อันดับ 1 พร้อมให้คุณค้นหาประสบการณ์ใหม่ ได้แล้ววันนี้ ที่ศูนย์ฮอนด้าทั่วประเทศไทย">
+    <meta property="og:image" content="">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="minimum-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no">
+	<meta name="csrf-token" content="PhE8OHElXW9DVqFwavOf1yRIdxZuIjjsh1WbNPNU">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+			
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script type="text/javascript" id="www-widgetapi-script" src="https://s.ytimg.com/yts/jsbin/www-widgetapi-vflTZdOF2/www-widgetapi.js" async=""></script><script src="https://www.youtube.com/iframe_api"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-30210512-3"></script>
+        <html xmlns="http://www.w3.org/1999/xhtml">
+
+<meta http-equiv="Content-Type" content="text/html; charset=tis-620" />
+
+<style type="text/css">
+
+* {margin:0;padding:0}
+
+</style>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+
+<!-- ขยายรูป -->
+<script>
+
+//<![CDATA[
+
+$(function(){
+
+ $('.imgx').hover(function(){
+
+  var w = 200;
+
+  var h = 220;
+
+  var d = 600;//duration
+
+  var imgx = $(this);
+
+  $('.imgy').remove();
+
+  var imgy = $('<img class="imgy" src="'+$(this).attr('src')+'"/>').appendTo('body');
+
+  imgy.css({
+
+   position: 'absolute',
+
+   left: imgx.offset().left,
+
+   top: imgx.offset().top,
+
+   width: imgx.width(),
+
+   height: imgx.height()
+
+   }).mouseout(function(){
+
+    $('.imgy').remove();
+
+   }).click(function(){
+
+    $('.imgy').remove();
+
+   });
+
+  imgy.animate({
+
+   left: imgx.offset().left - (w/2),
+
+   top: imgx.offset().top - (h/2),
+
+   width: w+'px',
+
+   height: h+'px'
+
+  },d);
+
+ },function(){});
+
+});
+
+//]]>
+
+</script>
+<!-- //ขยายรูป -->
+</head>
+
+<body>
+
+<div style="width:200px;height:200px;padding:100px;">
+
+<?php //foreach(range(1,117) as $i):?>
+
+<img src="mappin.png" class="imgx" />
+<img src="mappin.png" class="imgx" />
+
+<?php //endforeach;?>
+
+ </div>
+
+</body>
+
+</html>
